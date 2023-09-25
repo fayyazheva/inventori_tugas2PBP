77b94dda611e455cb600e9e0d75d2ce38567ce50
fayyazheva <fayya.salwa@ui.ac.id>
2023-09-25 14:24:56 +0700

    tutorial 3 selesai

--- main/models.py
+++ main/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     product = models.CharField(max_length=255)
     price = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # models.Model adalah kelas dasar yang digunakan untuk mendefinisikan model dalam Django.
 # Product adalah nama model yang kamu definisikan.
 # nama, tanggal_tambah, harga, dan deskripsi adalah atribut atau field pada model. Setiap field memiliki tipe data yang sesuai seperti CharField, DateField, IntegerField, dan TextField
+# user = kode diatas berfungsi untuk menghubungkan satu produk dengan satu user melalui sebuah relationship, dimana sebuah produk pasti terasosiasikan dengan seorang user. Lebih lanjutnya terkait ForeignKey akan dipelajari pada mata kuliah Basis Data. 
 
--- main/views.py
+++ main/views.py
@@ -5,26 +5,38 @@ from django.urls import reverse
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+@login_required(login_url='/login')
 
 def show_main(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
     context = {
-        'product': 'Mochi Paling enak se-Pacil',
-        'price': 'Rp 15.000 - Rp.30.000',
-        'description' : 'Mochi enak, sehat, cocok untuk anak fasilkom karena warnanya merah biru :D',
-        'amount' : '100',
+        'name' : 'Fayya Salwa Azheva',
+        'kelas' : 'PBP B',
+        'app_Name' : 'mochi', 
         'products' : products,
+        'last_login': request.COOKIES['last_login'],
+        'name': request.user.username,
     }
 
     return render(request, "main.html", context)
 
 def create_product(request):
     form = ProductForm(request.POST or None)
-
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
-
     context = {'form': form}
     return render(request, "create_product.html", context)
 
@@ -44,6 +56,39 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
 # def show_main(request) merupakan deklarasi fungsi show_main, yang menerima parameter request. Fungsi ini akan mengatur permintaan HTTP dan mengembalikan tampilan yang sesuai.
 # context adalah dictionary yang berisi data yang akan dikirimkan ke tampilan. Pada konteks ini, dua data disertakan, yaitu:
 
@@ -62,3 +107,26 @@ def show_json_by_id(request, id):
 
 # Fungsi Product.objects.all() digunakan untuk mengambil seluruh object Product yang tersimpan pada database.
 #return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+# form = UserCreationForm(request.POST) digunakan untuk membuat UserCreationForm baru dari yang sudah di-impor sebelumnya dengan memasukkan QueryDict berdasarkan input dari user pada request.POST.
+# form.is_valid() digunakan untuk memvalidasi isi input dari form tersebut.
+# form.save() digunakan untuk membuat dan menyimpan data dari form tersebut.
+# messages.success(request, 'Your account has been successfully created!') digunakan untuk menampilkan pesan kepada pengguna setelah melakukan suatu aksi.
+# return redirect('main:show_main') digunakan untuk melakukan redirect setelah data form berhasil disimpan.
+
+# Singkatnya, function authenticate, login yang diimport diatas akan digunakan untuk melakukan autentikasi dan login jika autentikasi berhasil
+
+# authenticate(request, username=username, password=password digunakan untuk melakukan autentikasi pengguna berdasarkan username dan password yang diterima dari permintaan (request) yang dikirim oleh pengguna saat login
+
+# logout(request) digunakan untuk menghapus sesi pengguna yang saat ini masuk.
+# return redirect('main:login') mengarahkan pengguna ke halaman login dalam aplikasi Django.
+
+# login(request, user) berfungsi untuk melakukan login terlebih dahulu
+# response = HttpResponseRedirect(reverse("main:show_main")) untuk membuat response
+# `response.setcookie('last_login', str(datetime.datetime.now())) berfungsi untuk membuat _cookie last_login dan menambahkannya ke dalam response
+
+# 'last_login': request.COOKIES['last_login'] berfungsi menambahkan informasi cookie last_login pada response yang akan ditampilkan di halaman web.
+
+# response.delete_cookie('last_login') berfungsi untuk menghapus cookie last_login saat pengguna melakukan logout
+
+# Parameter commit=False yang digunakan pada potongan kode diatas berguna untuk mencegah Django agar tidak langsung menyimpan objek yang telah dibuat dari form langsung ke database. Hal tersebut memungkinkan kita untuk memodifikasi terlebih dahulu objek tersebut sebelum disimpan ke database. Pada kasus ini, kita akan mengisi field user dengan objek User dari return value request.user yang sedang terotorisasi untuk menandakan bahwa objek tersebut dimiliki oleh pengguna yang sedang login.
